2b7bd53409455c6d4f3dd51810dc84507a6b2925
Kao <0980124jim@gmail.com>
2020-10-07 12:19:51 +0800

    hw3

--- homeworks/week13/hw2/api_add_comment.php
+++ homeworks/week13/hw2/api_add_comment.php
@@ -1,6 +1,7 @@
 <?php
   require_once("./conn.php");
   header("Content-type:application/json;charset:utf-8");
+  header("Access-Control-Allow-Origin: *");
   if(empty($_POST["comment"]) || empty($_POST["username"])) {
     $json = array(
       "ok" => false,
--- homeworks/week13/hw2/api_comments.php
+++ homeworks/week13/hw2/api_comments.php
@@ -1,4 +1,5 @@
 <?php
+  header("Access-Control-Allow-Origin: *");
   require_once("./conn.php");
   $page = 1;
   $site_key = "jim";
